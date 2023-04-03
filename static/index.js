055dfc01f61dce3fd496fedee90fbdbdaf078348
Tyler mui <tylermui1103@gmail.com>
2023-04-03 10:44:57 -0400

    more implementation

--- static/index.js
+++ static/index.js
@@ -1,6 +1,13 @@
 document.getElementById("add").addEventListener("click", function() {
-      var newTextbox = document.createElement("input");
-      newTextbox.type = "text";
-      var container = document.getElementById("addContainer");
-      container.appendChild(newTextbox);
+    var container = document.getElementById("addContainer");
+    var newSection = document.createElement("section");
+	var newH1 = document.createElement("h1");
+	newH1.contentEditable = true;
+	newH1.textContent = "New Section";
+	var newSubnotes = document.createElement("div");
+	newSubnotes.className = "subnotes";
+	newSubnotes.contentEditable = true;
+	newSection.appendChild(newH1);
+	newSection.appendChild(newSubnotes);
+	container.appendChild(newSection);
 });
