41e09148cbf7813c7ce9143ffee600ee3a2aad97
Tyler mui <tylermui1103@gmail.com>
2023-04-09 14:36:58 -0400

    login page and logout button

--- app.py
+++ app.py
@@ -7,24 +7,32 @@
 # source travel-env/bin/activate
 # flask --app app run 
 
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
+# Route to the main page
 @app.route("/")
 def home():
     return render_template("index.html")
 
 # Route for handling the login page logic
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     error = None
-#     if request.method == 'POST':
-#         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-#             error = 'Invalid Credentials. Please try again.'
-#         else:
-#             return redirect(url_for('home'))
-#     return render_template('login.html', error=error)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+# Route for handling the logout button
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
